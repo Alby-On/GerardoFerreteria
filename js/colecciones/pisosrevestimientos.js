c7800b5e272155d006d020cf43c3485ebea56f68
Alby-On <66146586+Alby-On@users.noreply.github.com>
2026-02-28 20:49:55 -0300

    Update pisosrevestimientos.js

--- js/colecciones/pisosrevestimientos.js
+++ js/colecciones/pisosrevestimientos.js
@@ -25,7 +25,7 @@
     ShopifyBuy.UI.onReady(client).then(function (ui) {
       ui.createComponent('collection', {
         id: '482436415713',
-        node: document.getElementById('collection-component-1772321220153'),
+        node: document.getElementById('shopify-products-load'),
         moneyFormat: '%24%7B%7Bamount_no_decimals%7D%7D',
         options: {
   "product": {
